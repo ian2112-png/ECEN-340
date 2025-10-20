555f1487795ffe39ff2350b43e57a71c600008ff
Seth Ricks <131173064+Sethricks340@users.noreply.github.com>
2025-10-20 12:43:47 -0600

    lab 6 is in lab 5 right now.
    lab 6 is in lab 5 right now. also the display is shifted to the left for some reason

---
+++ Week5 - Multiplier/Part 1/Lab6/Lab6.srcs/sources_1/new/clk_gen.v
@@ -0,0 +1,41 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 10/20/2025 11:49:50 AM
+// Design Name: 
+// Module Name: clk_gen
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module clk_gen (
+    input wire clk_in,         // 100 MHz input clock
+    input wire reset,
+    output wire clk_out        // divided clock output
+);
+
+    reg [25:0] counter = 0;
+
+    always @(posedge clk_in or posedge reset) begin
+        if (reset) begin
+            counter <= 0;
+        end else begin
+            counter <= counter + 1;
+        end
+    end
+
+    assign clk_out = counter[17];  // Using bit 17 for ~381 Hz clock
+
+endmodule
---
+++ Week5 - Multiplier/Part 1/Lab6/Lab6.srcs/sources_1/new/clock_dividers.v
@@ -0,0 +1,92 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 10/20/2025 10:21:37 AM
+// Design Name: 
+// Module Name: clock_dividers
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module clock_dividers(sw, clk, btnC, seg, an, dp);
+    input [15:0] sw; // TODO: how to set clock speed
+    input clk, btnC;
+    output reg [6:0] seg;
+    output reg [3:0] an;
+    output dp;
+    wire slow_clk;
+    
+    clk_gen clock_slow (.clk_in(clk), .reset(btnC), .clk_out(slow_clk));
+    
+    function [6:0] get_led_layout;
+        input [3:0] switches;
+        begin 
+            case (switches)
+                4'd0:  get_led_layout = 7'b1000000;    // 0
+                4'd1:  get_led_layout = 7'b1111001;    // 1
+                4'd2:  get_led_layout = 7'b0100100;    // 2
+                4'd3:  get_led_layout = 7'b0110000;    // 3
+                4'd4:  get_led_layout = 7'b0011001;    // 4
+                4'd5:  get_led_layout = 7'b0010010;    // 5
+                4'd6:  get_led_layout = 7'b0000011;    // 6
+                4'd7:  get_led_layout = 7'b1111000;    // 7
+                4'd8:  get_led_layout = 7'b0000000;    // 8
+                4'd9:  get_led_layout = 7'b0011000;    // 9
+                4'd10: get_led_layout = 7'b0001000;    // A
+                4'd11: get_led_layout = 7'b0000011;    // b
+                4'd12: get_led_layout = 7'b1000110;    // C
+                4'd13: get_led_layout = 7'b0100001;    // d
+                4'd14: get_led_layout = 7'b0000110;    // E
+                4'd15: get_led_layout = 7'b0001110;    // F
+                default: get_led_layout = 7'b1111111;  // All segments off
+            endcase
+        end
+    endfunction
+    
+    
+    always @ (posedge slow_clk, posedge btnC) begin
+    
+       if (btnC) begin
+           an <= 4'b1111;
+       end 
+       
+       else
+        begin 
+        case (an)
+            4'b1111: an <= 4'b1110; 
+            4'b1110: begin 
+                an <= 4'b1101;
+                seg <= get_led_layout(sw[3:0]); end
+            4'b1101: begin 
+                an <= 4'b1011;      
+                seg <= get_led_layout(sw[7:4]); end
+            4'b1011: begin 
+                an <= 4'b0111;    
+                seg <= get_led_layout(sw[11:8]); end
+            4'b0111: begin 
+                an <= 4'b1110; 
+                seg <= get_led_layout(sw[15:12]); end
+            default: begin 
+                an <= 4'b1111;
+                seg <= 7'b1111111; end // all segments off
+         endcase  
+               
+        end
+       end 
+         
+endmodule
+
+
+
